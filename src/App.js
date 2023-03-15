e5aa172ed6593b9401bd4a66264d35fd321a1948
Abhyuday <abhyuday911@gmail.com>
2023-03-15 14:20:33 +0530

    profile routing

--- src/App.js
+++ src/App.js
@@ -1,54 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Profile from "./Components/Profile";
 import "./Style/App.css";
 
 const App = () => {
   return (
     <div id="main">
-      <div className="left">
-        <div className="leftCircle"></div>
-        <div className="leftInput">
-          <input type="text" />
-        </div>
-        <Link to={"/"} href="#">Dashboard</Link>
-        <Link to={"/h"}>Profile</Link>
-        <Link to={"/h"}>Home</Link>
-        <Link to={"/h"}>Dashboard</Link>
-        <Link to={"/h"}>Dashboard</Link>
-        <hr className="lhr" />
-        <Link to={"/h"}>Dashboard</Link>
-        <Link to={"/h"}>Dashboard</Link>
-      </div>
-      <div className="right">
-        <div className="rtop">
-          <h1>Dashboard</h1>
-          <div className="rtopright">
-            <div className="prodiv">
-              <div className="lprodiv">
-                <div className="prodivcirc"></div>
-                <h3>John Doe</h3>
-              </div>
-              <div className="rprodiv"></div>
-            </div>
-          </div>
-        </div>
-        <div className="rbottom">
-          <div className="rbottleft">
-            <div className="fourcomp">
-              <div id="fourleft">
-                <div className="fourltop"></div>
-                <div className="fourlbott"></div>
-              </div>
-              <div id="charright">
-                <div id="halffour"></div>
-                <div id="halffour"></div>
-              </div>
-            </div>
-            <div className="fourbott"></div>
-          </div>
-          <div className="rbottreft"></div>
-        </div>
-      </div>
+      <Routes>
+        <Route path={"/"} element={<Home />}></Route>
+        <Route path={"/profile"} element={<Profile />}></Route>
+        
+      </Routes>
     </div>
   );
 };
---
+++ src/Components/Home.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <>
+    <div className="left">
+        <div className="leftCircle"></div>
+        <div className="leftInput">
+          <input type="text" />
+        </div>
+        <Link to={"/"} href="#">Dashboard</Link>
+        <Link to={"/profile"}>Profile</Link>
+        <Link to={"/h"}>Home</Link>
+        <Link to={"/h"}>Dashboard</Link>
+        <Link to={"/h"}>Dashboard</Link>
+        <hr className="lhr" />
+        <Link to={"/h"}>Dashboard</Link>
+        <Link to={"/h"}>Dashboard</Link>
+      </div>
+      <div className="right">
+        <div className="rtop">
+          <h1>Dashboard</h1>
+          <div className="rtopright">
+            <div className="prodiv">
+              <div className="lprodiv">
+                <div className="prodivcirc"></div>
+                <h3>John Doe</h3>
+              </div>
+              <div className="rprodiv"></div>
+            </div>
+          </div>
+        </div>
+        <div className="rbottom">
+          <div className="rbottleft">
+            <div className="fourcomp">
+              <div id="fourleft">
+                <div className="fourltop"></div>
+                <div className="fourlbott"></div>
+              </div>
+              <div id="charright">
+                <div id="halffour"></div>
+                <div id="halffour"></div>
+              </div>
+            </div>
+            <div className="fourbott"></div>
+          </div>
+          <div className="rbottreft"></div>
+        </div>
+      </div></>
+  )
+}
+
+export default Home
